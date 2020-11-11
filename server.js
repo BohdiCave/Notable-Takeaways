fc193753b2d0a671046fa59dd0c3141d71fe020e
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2020-11-10 23:49:34 -0500

    adding server.js and HTML and API routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express(); 
+ 
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
+
+app.listen(PORT, () => {
+  console.log("Server is listening on PORT: " + PORT);
+});
