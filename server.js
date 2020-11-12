b7d87726709de779abc14024aa602b150485ed37
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2020-11-12 06:15:00 -0500

    preparing to deploy to Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express(); 
  
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
